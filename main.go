189cf7a60f49e4a7c67ca1792794331681e3fc95
TJ Holowaychuk <tj@vision-media.ca>
2014-12-08 19:50:54 -0800

    add --top flag to exclude sub-packages from results

--- main.go
+++ main.go
@@ -19,11 +19,12 @@ var Version = "0.0.1"
 
 const Usage = `
   Usage:
-    go-search <query>...
+    go-search <query>... [--top]
     go-search -h | --help
     go-search --version
 
   Options:
+    -t, --top        top-level packages only
     -h, --help       output help information
     -v, --version    output version
 
@@ -34,6 +35,7 @@ func main() {
 	log.Check(err)
 
 	query := strings.Join(args["<query>"].([]string), " ")
+	top := args["--top"].(bool)
 
 	res, err := http.Get("http://api.godoc.org/search?q=" + url.QueryEscape(query))
 	if err != nil {
@@ -50,17 +52,34 @@ func main() {
 
 	println()
 	for _, pkg := range body.Results {
-		fmt.Printf("  \033[1m%s\033[m\n", strip(pkg.Path))
-		if pkg.Synopsis == "" {
-			fmt.Printf("  %s\n", "no description")
-		} else {
-			fmt.Printf("  %s\n", pkg.Synopsis)
+		if top && subpackage(pkg.Path) {
+			continue
 		}
+
+		fmt.Printf("  \033[1m%s\033[m\n", strip(pkg.Path))
+		fmt.Printf("  %s\n", description(pkg.Synopsis))
 		fmt.Printf("  godoc.org/pkg/%s\n\n", pkg.Path)
 	}
 	println()
 }
 
-func strip(path string) string {
-	return strings.Replace(path, "github.com/", "", 1)
+func subpackage(s string) bool {
+	switch {
+	case strings.HasPrefix(s, "github.com"):
+		return strings.Count(s, "/") > 3
+	default:
+		return false
+	}
+}
+
+func description(s string) string {
+	if s == "" {
+		return "no description"
+	}
+
+	return s
+}
+
+func strip(s string) string {
+	return strings.Replace(s, "github.com/", "", 1)
 }
